994de42ad160a0b5768fc6e58b9b39ced8c08b39
Patrik Nyman <p47nyman@gmail.com>
2021-11-03 11:54:19 +0100

    Day 07 klar.

--- 2020/day-07.pl
+++ 2020/day-07.pl
@@ -4,7 +4,7 @@ use Path::Tiny;
 
 my %bags =
   map { make_key( $_->[0] ) => make_value( $_->[1] ) }
-  map { [ split /contain/, s/bags|\.//gr ] }
+  map { [ split /contain/, s/bags?|\.//gr ] }
   split /\n/, path('input/day-07-input.txt')->slurp;
 
 sub make_key {
@@ -14,23 +14,51 @@ sub make_key {
 }
 
 sub make_value {
-    [ map { s/\d+//g; make_key($_) } split /,/, shift ]
+    my $val = shift;
+    my @result;
+    for my $bag ( split /,/, $val ) {
+        my $n = 0;
+        $bag =~ s/(\d+)//;
+        $n = $1;
+        push @result, { amount => $n, content => make_key($bag) };
+    }
+    \@result;
 }
-
 
 my %bagset;
 
-sub find_shiny_gold {
+sub part_1 {
     my $child = shift // 'shiny-gold';
     for ( keys %bags ) {
-        if ( grep /$child/, $bags{$_}->@* ) {
-            find_shiny_gold($_);
+        for my $bag ( $bags{$_}->@* ) {
+            next unless $child eq $bag->{content};
+            part_1($_);
             $bagset{$_}++;
         }
     }
 }
 
-find_shiny_gold;
+my $number_of_bags = 0;
+
+sub part_2 {
+    my $bag     = shift // 'shiny-gold';
+    my $lastbag = shift // 1;
+    for my $key ( keys %bags ) {
+        next unless $key eq $bag;
+        for my $subbag ( $bags{$bag}->@* ) {
+            next unless $subbag->{amount};
+            part_2( $subbag->{content}, $subbag->{amount} * $lastbag );
+            $number_of_bags += $subbag->{amount} * $lastbag;
+        }
+    }
+}
+
+part_1;
 print scalar keys %bagset, "\n";
 
 # 252
+
+part_2;
+print $number_of_bags, "\n";
+
+# 35487
