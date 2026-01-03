e9c88a152812b790aab13fa4e8151deae9a7c5d1
Patrik Nyman <p47nyman@gmail.com>
2026-01-03 08:21:55 +0100

    Modifierat day-08.pl

--- 2025/perl/day-08.pl
+++ 2025/perl/day-08.pl
@@ -10,12 +10,14 @@ sub get_input {
     map [ split /,/ ], path($input)->lines( { chomp => 1 } );
 }
 
-sub distance ( $p1, $p2 ) {
-    ## no need to square it
-    sum map { ( $p1->[$_] - $p2->[$_] )**2 } 0 .. 2;
+sub distance ( $a, $b ) {
+    my $dx = $a->[0] - $b->[0];
+    my $dy = $a->[1] - $b->[1];
+    my $dz = $a->[2] - $b->[2];
+    $dx * $dx + $dy * $dy + $dz * $dz;
 }
 
-sub sort_edges (@nodes) {
+sub make_edges (@nodes) {
     my @edges;
 
     for my $i ( 0 .. $#nodes - 1 ) {
@@ -35,31 +37,29 @@ sub part_1 ($edges) {
     my $g = Graph::Undirected->new;
     $g->add_edges( @$edges[ 0 .. 999 ] );
     my @s = sort { @$b <=> @$a } $g->connected_components;
-    say @{ $s[0] } * @{ $s[1] } * @{ $s[2] };
+    @{ $s[0] } * @{ $s[1] } * @{ $s[2] };
 }
 
-sub part_2 ( $edges, $nr_boxes ) {
-    my $g    = Graph::Undirected->new;
-    my %seen = ();
+sub part_2 ( $edges, $nr_nodes ) {
+    my $g = Graph::Undirected->new;
+    my %nodes;
 
     for my $edge (@$edges) {
         $g->add_edges($edge);
 
-        $seen{ $edge->[0] } = 1;
-        $seen{ $edge->[1] } = 1;
-        next if keys %seen < $nr_boxes;
+        $nodes{ $edge->[0] } = 1;
+        $nodes{ $edge->[1] } = 1;
+        next if keys %nodes < $nr_nodes;
 
         if ( $g->connected_components == 1 ) {
             my ($x1) = split /-/, $edge->[0];
             my ($x2) = split /-/, $edge->[1];
-            say $x1 * $x2;
-            return;
+            return $x1 * $x2;
         }
     }
 }
 
-my @input = get_input;
-my $edges = sort_edges(@input);
-
-part_1 $edges;                  # 62186
-part_2 $edges, scalar @input;   # 8420405530
+my @nodes = get_input;
+my $edges = make_edges(@nodes);
+say part_1 $edges;                   # 62186
+say part_2 $edges, scalar @nodes;    # 8420405530
